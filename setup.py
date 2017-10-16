89ffcd80f7e014c8f728d8341eb07fd3993c6382
Sreejith S <sreejith2904@gmail.com>
2017-10-16 00:21:29 -0400

    add requirements and setup file

---
+++ setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+from setuptools import setup
+from setuptools import find_packages
+
+install_requires = [
+    'pandas',
+    'scipy',
+    'scikit-learn',
+    'textstat',
+    'configparser',
+    'psycopg2',
+    'beautifulsoup4'
+]
+
+setup(
+      name='ares',
+      version='0.0.1',
+      description='Sentence Similarity Comparisons',
+      author='Sreejith Sreekumar',
+      author_email='sreekumar.s@husky.neu.edu',
+      url='https://github.com/srjit/ares',
+      license='GNU GPL v3',
+      install_requires=install_requires,
+      packages=find_packages()
+)
